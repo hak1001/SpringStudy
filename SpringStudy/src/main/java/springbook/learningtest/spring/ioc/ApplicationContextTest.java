f2f3fb57aa9e9799c7e50356169af2754e7e1b33
hak <hak@hak-PC>
2015-06-13 22:19:14 +0900

    컨텍스트 계층구조 테스트 추가

--- SpringStudy/src/main/java/springbook/learningtest/spring/ioc/ApplicationContextTest.java
+++ SpringStudy/src/main/java/springbook/learningtest/spring/ioc/ApplicationContextTest.java
@@ -5,18 +5,24 @@ import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
+import org.mockito.internal.matchers.NotNull;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.RuntimeBeanReference;
 import org.springframework.beans.factory.support.RootBeanDefinition;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.context.support.StaticApplicationContext;
+import org.springframework.util.ClassUtils;
+import org.springframework.util.StringUtils;
 
 import springbook.learningtest.spring.ioc.bean.Hello;
+import springbook.learningtest.spring.ioc.bean.Printer;
 import springbook.learningtest.spring.ioc.bean.StringPrinter;
 
 public class ApplicationContextTest {
+	private String basePath = StringUtils.cleanPath(ClassUtils.classPackageAsResourcePath(getClass())) + "/";
 	
 	@Test
 	public void registerBean() {
@@ -59,7 +65,7 @@ public class ApplicationContextTest {
 		GenericApplicationContext ac = new GenericApplicationContext();
 		
 		XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(ac);
-		reader.loadBeanDefinitions("springbook/learningtest/spring/ioc/genericApplicationContext.xml");
+		reader.loadBeanDefinitions(basePath + "genericApplicationContext.xml");
 		
 		//	모든 메타정보가 등록이 완료됐으니 애플리케이션 컨테이너를 초기화하는 명령어
 		ac.refresh();
@@ -72,12 +78,30 @@ public class ApplicationContextTest {
 	
 	@Test
 	public void genericXmlApplicationContext(){
-		GenericApplicationContext ac = new GenericXmlApplicationContext("springbook/learningtest/spring/ioc/genericApplicationContext.xml");
+		GenericApplicationContext ac = new GenericXmlApplicationContext(basePath +"genericApplicationContext.xml");
 		
 		Hello hello = ac.getBean("hello", Hello.class);
 		hello.print();
-		
 		assertThat(ac.getBean("printer").toString(), is("Hello Spring"));
+	}
+
+	@Test
+	public void contextHierachy(){
+		ApplicationContext parent = new GenericXmlApplicationContext(basePath + "parentContext.xml");
+		
+		GenericApplicationContext child = new GenericApplicationContext(parent);
+		XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(child);
+		reader.loadBeanDefinitions(basePath + "childContext.xml");
+		child.refresh();
+		
+		Printer printer = child.getBean("printer", Printer.class);
+		assertThat(printer, is(notNullValue()));
+		
+		Hello helloChild = child.getBean("hello", Hello.class);
+		assertThat(helloChild, is(notNullValue()));
+		
+		helloChild.print();
+		assertThat(printer.toString(), is("Hello Child"));
 	}
 	
 }
